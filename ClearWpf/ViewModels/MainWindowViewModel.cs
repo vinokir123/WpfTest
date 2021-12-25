1ba04ee4b3eddf8d3d8a0a268025db7cd7360e2f
PC\Кирилл <vinokir@mail.ru>
2021-12-25 16:28:04 +0700

    Добавлена MainWindowViewModel

---
+++ ClearWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ClearWpf.ViewModels.Base;
+
+namespace ClearWpf.ViewModels
+{
+    public class MainWindowViewModel : TittledViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Tittle = "Главное окно";
+        }
+    }
+}
