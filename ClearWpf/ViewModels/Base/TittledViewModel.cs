d763303f989cc73506bfdff21e98e5c2a4f01c35
PC\Кирилл <vinokir@mail.ru>
2021-12-25 16:01:08 +0700

    Добавлен класс TittledViewModel

---
+++ ClearWpf/ViewModels/Base/TittledViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearWpf.ViewModels.Base
+{
+    public abstract class TittledViewModel : ViewModel
+    {
+        #region Tittle : string - Заголовок
+
+        /// <summary>Заголовок</summary>
+        private string _Tittle;
+
+        /// <summary>Заголовок</summary>
+        public string Tittle { get => _Tittle; set => Set(ref _Tittle, value); }
+
+        #endregion
+    }
+}
